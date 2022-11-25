de255da21bd00da623dba98116a99e8de29c9d2f
annt <annt@gft.com>
2022-11-25 12:54:50 +0000

    Done with System.arraycopy

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class Solution {
+
+    public Solution(){
+
+    }
+
+
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int[] result = new int[m + n];
+
+        System.arraycopy(nums1, 0, result, 0, m);
+        System.arraycopy(nums2, 0, result, m, n);
+
+        Arrays.sort(result);
+
+        return result;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,67 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    static int[] nums1, nums2, result;
+    static int n,m;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+        nums1 = new int[m];
+        nums2 = new int[n];
+
+    }
+
+    @Test
+    public void mergeTwoArraysWith0ElementsEach(){
+
+        n = 0;
+        m = 0;
+
+        result = new int[] {};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.merge(nums1, m, nums2, n)));
+
+    }
+
+    @Test
+    public void mergeTwoArraysWith3ElementsEach(){
+
+        n = 3;
+        m = 3;
+
+        nums1 = new int[] {2,4,6};
+        nums2 = new int[] {1,3,9};
+
+
+        result = new int[] {1,2,3,4,6,9};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.merge(nums1, m, nums2, n)));
+    }
+
+    @Test
+    public void mergeTwoArraysWith5ElementsFirstAnd2TheOther(){
+
+        n = 2;
+        m = 5;
+
+        nums1 = new int[] {2,4,6,8,11};
+        nums2 = new int[] {1,9};
+
+
+        result = new int[] {1,2,4,6,8,9,11};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.merge(nums1, m, nums2, n)));
+    }
 }
