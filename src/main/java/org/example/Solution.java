bc14da180905d4aa41731454484a167e2ebb47d8
annt <annt@gft.com>
2022-11-28 09:50:41 +0000

    small change

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -23,7 +23,10 @@ public class Solution {
 
     public void mergeSameArray(int[] nums1, int m, int[] nums2, int n) {
 
-        if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
+        if (n >= 0){
+            
+            System.arraycopy(nums2, 0, nums1, m, n);
+        }
 
         Arrays.sort(nums1);
     }
